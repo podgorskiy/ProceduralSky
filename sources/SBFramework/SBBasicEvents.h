38231b8aedc05c68409b1d343d1607b0db93db0d
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2015-11-20 04:37:39 -0500

    Moved camera free flight controller API and  imguibinding to framework

--- sources/SBFramework/SBBasicEvents.h
+++ sources/SBFramework/SBBasicEvents.h
@@ -1,40 +1,43 @@
 #pragma once
 
-namespace Event
+namespace SB
 {
-	enum ButtonType
+	namespace BasicEvents
 	{
-		MOUSE_BUTTON_NONE = -1,
-		MOUSE_BUTTON_LEFT = 0,
-		MOUSE_BUTTON_RIGHT = 1,
-		MOUSE_BUTTON_MIDDLE = 2,
+		enum ButtonType
+		{
+			MOUSE_BUTTON_NONE = -1,
+			MOUSE_BUTTON_LEFT = 0,
+			MOUSE_BUTTON_RIGHT = 1,
+			MOUSE_BUTTON_MIDDLE = 2,
 
-		KEY_W,
-		KEY_A,
-		KEY_S,
-		KEY_D
-	};
-	enum ActionType
-	{
-		ACTION_NONE = -1,
-		ACTION_PRESS = 0,
-		ACTION_RELEASE = 1
-	};
-	struct OnMouseButtonEvent
-	{
-		ButtonType button;
-		ActionType action;
-	};
+			KEY_W,
+			KEY_A,
+			KEY_S,
+			KEY_D
+		};
+		enum ActionType
+		{
+			ACTION_NONE = -1,
+			ACTION_PRESS = 0,
+			ACTION_RELEASE = 1
+		};
+		struct OnMouseButtonEvent
+		{
+			ButtonType button;
+			ActionType action;
+		};
 
-	struct OnMouseMoveEvent
-	{
-		float x;
-		float y;
-	};
-	struct OnKeyEvent
-	{
-		ButtonType key;
-		int rawKey;
-		ActionType action;
-	};
+		struct OnMouseMoveEvent
+		{
+			float x;
+			float y;
+		};
+		struct OnKeyEvent
+		{
+			ButtonType key;
+			int rawKey;
+			ActionType action;
+		};
+	}
 }
--- sources/SBFramework/SBImGuiBinding.h
+++ sources/SBFramework/SBImGuiBinding.h
@@ -1,33 +1,39 @@
 #pragma once
 #include "SBEventManager.h"
-#include "Events.h"
+#include "SBBasicEvents.h"
 
 namespace SB
 {
+	struct ScreenBufferSizes
+	{
+		int m_framebufferWidth;
+		int m_framebufferHeight;
+		int m_windowWidth;
+		int m_windowHeight;
+	};
+
 	class Shader;
+
+	class ImGuiBinding :
+		public SB::EventReceiver<BasicEvents::OnMouseButtonEvent>,
+		public SB::EventReceiver<BasicEvents::OnMouseMoveEvent>,
+		public SB::EventReceiver<BasicEvents::OnKeyEvent>
+	{
+	public:
+		void Init();
+		void NewFrame(const ScreenBufferSizes& screenBufferSizes);
+		void ShutDown();
+		void Render();
+
+		void EventReceiver_OnEvent(const BasicEvents::OnMouseButtonEvent& mouseButtonEvent);
+		void EventReceiver_OnEvent(const BasicEvents::OnMouseMoveEvent& mouseButtonEvent);
+		void EventReceiver_OnEvent(const BasicEvents::OnKeyEvent& keyEvent);
+
+	private:
+		SB::Shader* m_shader;
+		int u_texture;
+		int u_projMtx;
+		unsigned int m_vboHandles[2];
+		unsigned int m_fontTexture;
+	};
 }
-
-struct ScreenBufferSizes;
-
-class ImGuiBinding :
-	public SB::EventReceiver<Event::OnMouseButtonEvent>,
-	public SB::EventReceiver<Event::OnMouseMoveEvent>,
-	public SB::EventReceiver<Event::OnKeyEvent>
-{
-public:
-	void Init();
-	void NewFrame(const ScreenBufferSizes& screenBufferSizes);
-	void ShutDown();
-	void Render();
-
-	void EventReceiver_OnEvent(const Event::OnMouseButtonEvent& mouseButtonEvent);
-	void EventReceiver_OnEvent(const Event::OnMouseMoveEvent& mouseButtonEvent);
-	void EventReceiver_OnEvent(const Event::OnKeyEvent& keyEvent);
-
-private:
-	SB::Shader* m_shader;
-	int u_texture;
-	int u_projMtx;
-	unsigned int m_vboHandles[2];
-	unsigned int m_fontTexture;
-};
