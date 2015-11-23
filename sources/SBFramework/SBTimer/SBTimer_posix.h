d200301ad91559b162fa21a29af209b76a27b6c5
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2015-11-23 07:22:25 -0500

    +Added emscripten implementstion for timer

--- sources/SBFramework/SBTimer/SBTimer_posix.h
+++ sources/SBFramework/SBTimer/SBTimer_posix.h
@@ -2,14 +2,15 @@
 
 namespace SB
 {
-	long long GetTicks() 
+	unsigned long long GetTicks() 
 	{
 		struct timeval tp;
 		gettimeofday(&tp, 0);
 		return tp.tv_sec * 1000000 + tp.tv_usec;
 	}
 
-	long long GetTicksPerSecond() {
+	unsigned long long GetTicksPerSecond() 
+	{
 		return 1000000;
 	}
 }
